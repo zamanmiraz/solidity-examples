b97677479b5dd85111795ebc2ee3e82de37d5f17
Miraz Uz Zaman <40580823+zamanmiraz@users.noreply.github.com>
2023-11-07 22:30:52 -0600

    Update simpleContract.sol

--- solidity-linkedin-course/chapter2/contracts/simpleContract.sol
+++ solidity-linkedin-course/chapter2/contracts/simpleContract.sol
@@ -6,16 +6,20 @@ pragma solidity ^0.8.0;
 
 // first contract
 contract InheritanceContract {
+    // State varaibles
+    // An address payable variable that stores the address of the designated recipient who will receive funds in case the owner passes away.
     address payable public recipient;
+    // An address variable that stores the address of the contract owner.
     address public owner;
+    // A boolean variable that indicates whether the owner has been declared deceased.
     bool public isOwnerDecessed;
-
+    // Constructor is executed only once during the deployment of contract. During the deployment the contract owner and initial values for are set.
     constructor(address payable _recipient){
         owner = msg.sender;
         recipient = _recipient;
         isOwnerDecessed = false;
     }
-
+    //  Modifiere restricts access to function that are only accessible by the owner and recipient.
     modifier onlyOwner() {
         require(msg.sender == owner, "Only Contract owner can access this function");
         _;
@@ -25,7 +29,7 @@ contract InheritanceContract {
         require(msg.sender == recipient, "Only designated recipient can access this function");
         _;
     }
-
+    // Function is designed to call by the owener. If owner decessed and balance greater than zero, the contracts transfer the balance to the designated recipient.
     function declareDecessed() external onlyOwner{
         require(isOwnerDecessed, "Owner hash been declared deceased");
         require(address(this).balance > 0, "No funds availabe for withdrawl");
